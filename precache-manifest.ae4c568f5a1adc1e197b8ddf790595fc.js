176d2d86c3e54d77e7ec2092fdb0b7819131d8cb
Jodi Pranata <jodranata@gmail.com>
2019-12-24 19:50:58 +0700

    Updates

---
+++ precache-manifest.ae4c568f5a1adc1e197b8ddf790595fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd06c188f001e82c220eea86b2a584e",
+    "url": "/react-weatherforecastapp/index.html"
+  },
+  {
+    "revision": "08762b46657f175cc9ac",
+    "url": "/react-weatherforecastapp/static/css/main.9c425062.chunk.css"
+  },
+  {
+    "revision": "647e0c5f96956ec77fb5",
+    "url": "/react-weatherforecastapp/static/js/2.b3409c40.chunk.js"
+  },
+  {
+    "revision": "08762b46657f175cc9ac",
+    "url": "/react-weatherforecastapp/static/js/main.448fcd54.chunk.js"
+  },
+  {
+    "revision": "12962203d1d7cfeb0ae4",
+    "url": "/react-weatherforecastapp/static/js/runtime-main.a08af721.js"
+  },
+  {
+    "revision": "3efa3321f6d6c9b41c4a902d13bf1efb",
+    "url": "/react-weatherforecastapp/static/media/sunny.3efa3321.jpg"
+  }
+]);
